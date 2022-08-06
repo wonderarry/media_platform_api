c93687c087c65f9b36d98f8d5496add29ee95f6a
wonderarry <wonderarry@gmail.com>
2022-08-07 02:18:56 +0300

    created initial alembic version

---
+++ alembic/versions/cfa2fa5970e1_create_user_table.py
@@ -0,0 +1,24 @@
+"""create user table
+
+Revision ID: cfa2fa5970e1
+Revises: 
+Create Date: 2022-08-07 02:12:11.098965
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cfa2fa5970e1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
